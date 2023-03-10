91d1ad246dcc3fc379929395a9d2c127629c1288
StrangePineTree <97989279+StrangePineTree@users.noreply.github.com>
2023-03-10 14:19:20 -0700

    Update space invadors===.py

--- space invadors===.py
+++ space invadors===.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
@@ -8,6 +9,24 @@ player = pygame.image.load('player.png')
 bg = pygame.image.load("su back grondu.png")
 bg = pygame.transform.scale(bg,(800,800))
 timmer = 0
+
+class Missile:
+    def __init__(self):
+        self.pos = pygame.math.Vector2(-10,-10)
+        self.alive = True
+    def draw(self):
+        pygame.draw.rect(screen,(200,20,1),(self.pos, (3,20)))
+    def move(self):
+        if self.alive:
+            self.pos.y+=10
+        else:
+            self.pos = pygame.math.Vector2(-10,-10)
+        if self.pos.y > 800:
+            self.alive = False
+
+missiles: list[Missile] = []
+for i in range (10):
+    missiles.append(Missile())
 
 class Bullet:
     def __init__(self,x,y):
@@ -102,6 +121,16 @@ while running:
         if keys[pygame.K_d]:
             pos.x +=3
 
+
+    rando = random.randrange(0,100)
+    if rando < 2:
+        rando = random.randrange(0,len(anemyList))
+        if anemyList[rando].alive == True:
+            for rocket in missiles:
+                if rocket.alive == False:
+                    rocket.alive = True
+                    rocket.pos = anemyList[rando].pos
+                    break
     bullet.move()
     if bullet.alive == True:
         for alliin in anemyList:
@@ -114,6 +143,23 @@ while running:
         allien.move(timmer)
         if allien.alive == True:
             allien.draw()
+
+    for missile in missiles:
+        if missile.alive:
+            for wall in wolllist:
+                if missile.pos.x > wall.pos.x:
+                    if missile.pos.x < wall.pos.x + 30:
+                        if missile.pos.y < wall.pos.y + 30:
+                            if missile.pos.y > wall.pos.y:  
+                                wall.hp +=1
+                                missile.alive = False  
+        if missile.pos.x > pos.x:
+            if missile.pos.x < pos.x + 30:
+                if missile.pos.y < pos.y + 30:
+                    if missile.pos.y > pos.y:      
+                        print("ded player")   
+                        missile.alive = False 
+
     if bullet.alive == True:
         bullet.draw()
     for wall in wolllist:
@@ -124,6 +170,10 @@ while running:
     for wall in wolllist:
         if wall.alive:
             wall.collide()
+    for rocket in missiles:
+        rocket.move()
+        if rocket.alive == True:
+            rocket.draw()
 
     screen.blit(player,(pos))
 
